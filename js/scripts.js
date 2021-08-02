5d416accbfdf8e8d90b3e7aced4b9fdd37ade1c5
Tim Koprivnik <timkoprivnik@Tims-MacBook-Air.local>
2021-08-02 17:13:18 +0200

    finished

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,34 @@
-// console.log('test');
-
 // jQuery(document).ready(function() {
-//     console.log('test jquery');
-
-//     jQuery('#about-me-img').before('<img src="./themes/adchallenge/images/Rectangle13.png" id="about-me-img-before"/>');
-//     jQuery('#about-me-img').after('<img src="./themes/adchallenge/images/Rectangle14.png" id="about-me-img-after"/>');
 // });
+
+
+// FIXED ANCHORING
+(function($, window) {
+    // console.log('test jquery');
+
+    // jQuery('#about-me-img').before('<img src="./themes/adchallenge/images/Rectangle13.png" id="about-me-img-before"/>');
+    // jQuery('#about-me-img').after('<img src="./themes/adchallenge/images/Rectangle14.png" id="about-me-img-after"/>');
+    
+    // FIXED ANCHOR-ING
+    var adjustAnchor = function() {
+
+        var $anchor = $(':target'),
+            fixedElementHeight = 202; // *** 99 (header height) + 103 (content padding)
+
+        if ($anchor.length > 0) {
+            $('html, body')
+                // .stop()
+                // .animate({
+                //     scrollTop: $anchor.offset().top - fixedElementHeight
+                // }, 200);
+                window.scrollTo(0, $anchor.offset().top - fixedElementHeight);
+        }
+    };
+
+    $(window).on('hashchange load', function() {
+        adjustAnchor();
+    });
+
+})(jQuery, window);
+
+
